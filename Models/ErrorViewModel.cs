2998a735db287390cd866f424c2928abf1ab9aff
Iselin R <iselin.rolness03@gmail.com>
2025-10-01 22:40:36 +0200

    lagt inn enkle kommentarer i koden

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,7 +2,8 @@ namespace KartverketRegister.Models;
 
 public class ErrorViewModel
 {
+    //ID for forespørselen (kan være null)
     public string? RequestId { get; set; }
-
+    //sjekker at RequestID finnes, returnerer true (ikke tom eller null)
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
