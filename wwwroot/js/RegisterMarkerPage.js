0d372a0d3d2491d870cab1a957c1367765b6794c
J6kub <jakubb@uia.no>
2025-10-07 13:46:08 +0200

    Lagt til bedre respons ved marker registrering

---
+++ wwwroot/js/RegisterMarkerPage.js
@@ -0,0 +1,38 @@
+﻿const form = document.querySelector("form");
+const outputField = document.getElementById('returnMsg');
+const submitButton = document.querySelector('button[type="submit"]');
+
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault(); // Stop the default form submission
+    const formData = new FormData(form);
+
+    const response = await fetch(form.action, {
+        method: form.method.toUpperCase(), // "POST"
+        body: formData
+    }).then(response => {
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+        return response.json(); 
+    })
+        .then(reply => { 
+            outputField.innerHTML = reply.message;
+            if (reply.success) {
+                SuccessfulReq();
+            } else {
+                FailedReq();
+            }
+
+        })
+});
+
+function SuccessfulReq(msg) {
+    submitButton.disabled = true;
+    outputField.innerHTML += " - Redirecting to back to home . . .";
+
+    setTimeout(() => {
+        location.href = '/';
+    }, 1500)
+}
+function FailedReq() {
+
+}
