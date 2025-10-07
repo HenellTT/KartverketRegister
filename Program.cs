0edb2c2ad098b7520914c6b8d2a6cb1cc62197d7
J6kub <jakubb@uia.no>
2025-10-07 12:49:11 +0200

    Lagt til error handling for manglende kobling til database. Nå starter ikke appen før den kobler seg ordentlig til databasen og setter den opp

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using KartverketRegister.Utils;
+using System.Threading;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,10 +27,24 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
-SequelInit seq = new SequelInit(Constants.DataBaseIp, Constants.DataBaseName); // Funny but real, men programmet kræsjer hvis den ikke får kobla opp mot database. Så teknisk sett er det et test i seg selv
-seq.conn.Open();
-seq.InitDb();
-seq.conn.Close();
+SequelInit seq;
+bool ConnectedToDb = false;
+while (!ConnectedToDb)
+{
+    try
+    {
+        seq = new SequelInit(Constants.DataBaseIp, Constants.DataBaseName);
+        seq.conn.Open();
+        seq.InitDb();
+        seq.conn.Close();
+        // Funny but real, men programmet kræsjer hvis den ikke får kobla opp mot database. Så teknisk sett er det et test i seg selv
+        ConnectedToDb = true;
+    }
+    catch (Exception ex) {
+        Console.WriteLine($"Connection to db failed: No database found at {Constants.DataBaseIp}:{Constants.DataBasePort}");
+        Console.WriteLine("Retrying in 2s. . .");
+        Thread.Sleep(2000);
+    }
+}
 
 app.Run();
