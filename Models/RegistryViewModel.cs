7340ae5666e5e32f4087bedabc7fd5be1c19f436
iselin <iselin.rolness03@gmail.com>
2025-10-18 22:23:38 +0200

    Henter markørene og lagt de i lister

---
+++ Models/RegistryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace KartverketRegister.Models
+{
+    public class RegistryViewModel
+    {
+        public List<Marker> Markers { get; set; }
+        public List<TempMarker> TempMarkers { get; set; }
+    }
+}
