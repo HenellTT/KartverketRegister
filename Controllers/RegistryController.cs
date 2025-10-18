0b7901f6097bcf5ad0fa6ac05485676b892b9d2f
iselin <iselin.rolness03@gmail.com>
2025-10-18 22:22:33 +0200

    legger til RegistryController med visning av markører og midlertidige markører

--- Controllers/RegistryController.cs
+++ Controllers/RegistryController.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+using KartverketRegister.Models;
+using KartverketRegister.Utils;
+using KartverketRegister.Data;
+using System.Collections.Generic;
+
+namespace KartverketRegister.Controllers
+{
+    public class RegistryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RegistryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Registry()
+        {
+            var Markers = _context.Markers.ToList();
+            var TempMarkers = _context.TempMarkers.ToList();
+
+            var viewModel = new RegistryViewModel
+            {
+                Markers = Markers,
+                TempMarkers = TempMarkers
+            };
+
+            // Explicit path to the existing view under Views/Home
+            return View("~/Views/Home/Registry.cshtml", viewModel);
+        }
+    }
+}
