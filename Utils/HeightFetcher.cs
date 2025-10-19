cc048e3fa743e13184fe3eccea6c24766c792e10
J6kub <jakubb@uia.no>
2025-10-19 13:43:18 +0200

    Lagt til API controller som innhenter Ho/yde data for spesifisisiert lokasjon. Settet opp for fremtidlig innhenting av andre luftfarthindre fra NLA

---
+++ Utils/HeightFetcher.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+
+namespace KartverketRegister.Utils
+{
+    public static class HeightFetcher
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public class HoydeResponse
+        {
+            public string value { get; set; } // matches the JSON field "value"
+        }
+
+        public static async Task<double?> GetHeightFromUrlAsync(string url)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (!response.IsSuccessStatusCode) return null;
+
+                string json = await response.Content.ReadAsStringAsync();
+                var hoyde = JsonSerializer.Deserialize<HoydeResponse>(json);
+
+                if (hoyde == null || string.IsNullOrEmpty(hoyde.value)) return null;
+
+                if (double.TryParse(hoyde.value, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out double height))
+                {
+                    return height;
+                }
+
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Utils/LinkGeneratorBrr.cs
@@ -0,0 +1,29 @@
+﻿using KartverketRegister.Models.Other;
+using System.Text.Json;
+using System.Web;
+
+namespace KartverketRegister.Utils
+{
+    public static class LinkGeneratorBrr
+    {
+
+        public static string HoydeDataCoords(double e, double n) // e = x  -||- n = y
+        {
+            Geometry gm = new Geometry
+            {
+                x = e,
+                y = n,
+                spatialReference = new SpatialReference { wkid = 25833 }
+            };
+
+            string jsonGm = JsonSerializer.Serialize(gm);
+            string encodedJson = HttpUtility.UrlEncode(jsonGm);
+
+            string url = $"https://hoydedata.no/arcgis/rest/services/NHM_DOM_25833/ImageServer/identify?f=json&geometry={encodedJson}&geometryType=esriGeometryPoint&returnFieldName=false&returnGeometry=false&returnUnformattedValues=false&returnZ=false&layers=top";
+
+
+
+            return url;
+        }
+    }
+}
