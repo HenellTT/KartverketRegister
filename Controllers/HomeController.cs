76a14721c39abcc4c16d100e88bcecfc707eefe4
laurieniyongabo <laurieniyongabo@gmail.com>
2025-11-01 14:08:49 +0100

    Lagt til metoder for edit og view marker

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,26 +1,17 @@
-using KartverketRegister.Auth;
-using KartverketRegister.Models;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using KartverketRegister.Models;
+using KartverketRegister.Utils;
 
 namespace KartverketRegister.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-    private readonly UserManager<AppUser> _userManager;
-    private readonly SignInManager<AppUser> _signInManager;
 
-    public HomeController(
-        ILogger<HomeController> logger,
-        UserManager<AppUser> userManager,
-        SignInManager<AppUser> signInManager)
+    public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
-        _userManager = userManager;
-        _signInManager = signInManager;
     }
 
     public IActionResult Index()
@@ -33,27 +24,14 @@ public class HomeController : Controller
         return View(); //returnerer viewet Privacy.cshtml (personvernsiden)
     }
 
-    public IActionResult Registry()
+    /*public IActionResult Registry()
     {
         return View(); //returnerer viewet Registry.cshtml (registersiden)
-    }
+    }*/
 
-    public async Task<IActionResult> User()
+    public IActionResult User()
     {
-
-        try
-        {
-            var appUser = await _userManager.GetUserAsync(HttpContext?.User);
-            if (appUser != null)
-                return View("UserLogged", appUser);
-        } catch
-        {
-            return View(); //returnerer viewet User.cshtml (brukersiden)
-
-        }
-
-        return View();
-
+        return View(); //returnerer viewet User.cshtml (brukersiden)
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -61,4 +39,45 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }); 
     }
+
+    public IActionResult Registry()
+    {
+        SequelMarker seq = new SequelMarker(Constants.DataBaseIp, Constants.DataBaseName);
+        try
+        {
+            List<Marker> myMarkers = seq.FetchMyMarkers(1);
+            return View(myMarkers);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return View(new List<Marker>());
+        }
+    }
+    
+    [Route("EditMarker/{id:int}")]
+    public IActionResult EditMarker(int id)
+    {
+        SequelMarker seq = new SequelMarker(Constants.DataBaseIp, Constants.DataBaseName);
+        Marker marker = seq.FetchMarkerById(id);
+        return View(marker);
+    }
+    
+    [Route("ViewMarker/{id:int}")]
+    public IActionResult ViewMarker(int id)
+    {
+        SequelMarker seq = new SequelMarker(Constants.DataBaseIp, Constants.DataBaseName);
+        Marker marker = seq.FetchMarkerById(id);
+        return View(marker);
+        
+    }
+
+    /*public IActionResult UserView()
+    {
+        int userId = 1; 
+
+        SequelMarker seq = new SequelMarker(Constants.DataBaseIp, Constants.DataBaseName);
+        List<Marker> userMarkers = seq.FetchMarkersByUserId(userId);
+        return View(userMarkers);
+    }*/
 }
