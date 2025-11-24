be5fa5652afa7f2c429dbdcf895547d42b8b9da9
Iselin R <iselinro@uia.no>
2025-11-24 13:46:14 +0100

    fikset databasepassord

--- Utils/Constants.cs
+++ Utils/Constants.cs
@@ -5,7 +5,7 @@
     {
 	public static string DataBaseIp { get; } = Environment.GetEnvironmentVariable("DATABASE_IP") ?? "127.0.0.1"; 
 		public static int DataBasePort { get; } = int.TryParse(Environment.GetEnvironmentVariable("DATABASE_PORT"), out var port) ? port : 3306;
-        public static string DataBaseRootPassword { get; set; } = Environment.GetEnvironmentVariable("DATABASE_PASSWORD") ?? "7257";
+        public static string DataBaseRootPassword { get; set; } = Environment.GetEnvironmentVariable("DATABASE_PASSWORD") ?? "mysecretpassword";
         public static string DataBaseName { get; } = "ObstacleRegister";
         public static bool ResetDbOnStartup { get; set; } = false; // RETIRED; REPLACED BY MIGRATION
         // Kan forlenge startup tider ved større database. Men grei for innlevering.
