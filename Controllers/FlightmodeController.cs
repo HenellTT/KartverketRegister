2b7c75c33a29b685a412e83b3fc2c5e59086a6f0
laurieniyongabo <laurieniyongabo@gmail.com>
2025-11-28 11:47:22 +0100

    Forbedret FlightmodeController: fjernet ubrukte usings, ryddet opp

--- Controllers/FlightmodeController.cs
+++ Controllers/FlightmodeController.cs
@@ -1,21 +1,13 @@
-using KartverketRegister.Utils;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Runtime.CompilerServices;
-
 
 namespace KartverketRegister.Controllers
 {
+    // Flymodus-visning - forenklet kartvisning for piloter under flyging
     [Authorize(Roles = "User,Employee,Admin")]
-
     public class FlightmodeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View("FlightMode"); //returnerer viewet FlightMode.cshtml (FlightMode siden)
-		}
-    
+        [HttpGet]
+        public IActionResult Index() => View("FlightMode");
     }
 }
